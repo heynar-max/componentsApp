c8dcba3d8daa8ab96ae50a2f9a28eae727f90292
heynar-max <heynar76@hotmail.com>
2025-07-13 10:26:23 -0500

    Componente - Alert

---
+++ src/presentation/screens/alerts/AlertScreen.tsx
@@ -0,0 +1,87 @@
+import {Alert,StyleSheet,Text, View} from 'react-native';
+
+
+import {CustomView} from '../../components/ui/CustomView';
+import {Title} from '../../components/ui/Title';
+import {globalStyles} from '../../../config/theme/theme';
+import {Button} from '../../components/ui/Button';
+
+
+export const AlertScreen = () => {
+    const createTwoButtonAlert = () => {
+        Alert.alert(
+        'Alert Title',
+        'My Alert Msg',
+        [
+            {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+            },
+            {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ],
+        {},
+        );
+    };
+
+    const createThreeButtonAlert = () =>
+        Alert.alert(
+        'Alert Title',
+        'My Alert Msg',
+        [
+            {text: 'OK', onPress: () => console.log('OK Pressed')},
+            {
+            text: 'Ask me later',
+            onPress: () => console.log('Ask me later pressed'),
+            },
+            {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'destructive',
+            },
+        ],
+        {
+            cancelable: true,
+            onDismiss() {
+            console.log('onDismiss');
+            },
+        },
+        );
+
+    
+    
+
+        // ! Código nativo
+        // Alert.prompt(
+        //   'Correo electronico',
+        //   'Enim commodo ut amet esse aliqua.',
+        //   (valor: string) => console.log({valor}),
+        //   'secure-text',
+        //   'Soy el valor por defecto',
+        //   'number-pad'
+        // );
+
+    
+
+    return (
+        <CustomView style={globalStyles.globalMargin}>
+        <Title safe text="Alertas" />
+
+        <Button text="Alerta - 2 Botones" onPress={createTwoButtonAlert} />
+
+        <View style={styles.AlertView} />
+
+        <Button text="Alerta - 3 Botones" onPress={createThreeButtonAlert} />
+
+        <View style={styles.AlertView} />
+
+        <Button text="Prompt - Input" onPress={createThreeButtonAlert} />
+        </CustomView>
+    );
+};
+
+const styles = StyleSheet.create({
+    AlertView: {
+        height: 10,
+    },
+});
