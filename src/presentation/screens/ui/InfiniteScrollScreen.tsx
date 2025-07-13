f07d0bacad072e445df3d91d1d32dec803233168
heynar-max <heynar76@hotmail.com>
2025-07-13 12:45:58 -0500

    InfiniteScroll con imágenes

--- src/presentation/screens/ui/InfiniteScrollScreen.tsx
+++ src/presentation/screens/ui/InfiniteScrollScreen.tsx
@@ -1,43 +1,64 @@
-import {useState} from 'react';
-import { Text, View} from 'react-native';
-
-import {FlatList} from 'react-native-gesture-handler';
-import {colors} from '../../../config/theme/theme';
+import { useState } from 'react';
+import { ActivityIndicator, FlatList, Image, StyleSheet, View } from 'react-native';
+import { colors } from '../../../config/theme/theme';
 
 
 export const InfiniteScrollScreen = () => {
     const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
 
     const loadMore = () => {
-        const newArray = Array.from({length: 5}, (_, i) => numbers.length + i);
-
-        
+        const newArray = Array.from({ length: 5 }, (_, i) => numbers.length + i);
+        setTimeout(() => {
         setNumbers([...numbers, ...newArray]);
-        ;
+        }, 3000);
     };
 
     return (
-        <View style={{backgroundColor: 'black'}}>
+        <View style={styles.container}>
 
             {/* flatlist entran cuando estan en pantalla  */}
         <FlatList
             data={numbers}
             onEndReached={loadMore}
             onEndReachedThreshold={0.6}
-            
-            renderItem={({item}) => (
-                <Text style= {{
-                    height: 300,
-                    backgroundColor: colors.primary,
-                    color: 'white',
-                    fontSize: 50
-                }}>
-                    {item}
-                </Text>
-            )}
+            keyExtractor={(item) => item.toString()}
+            renderItem={({ item }) => <ListItem number={item} />}
+            ListFooterComponent={ListFooter}
         />
         </View>
     );
     };
 
-    
+    interface ListItemProps {
+    number: number;
+    }
+
+    const ListItem = ({ number }: ListItemProps) => {
+    return (
+        <Image
+        source={{uri: `https://picsum.photos/id/${number}/500/400`}}
+        style={styles.image}
+        />
+    );
+    };
+
+    const ListFooter = () => (
+    <View style={styles.footer}>
+        <ActivityIndicator size={40} color={colors.primary} />
+    </View>
+);
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'black',
+        flex: 1,
+    },
+    image: {
+        height: 400,
+        width: '100%',
+    },
+    footer: {
+        height: 150,
+        justifyContent: 'center',
+    },
+});
