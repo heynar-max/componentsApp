3073462819d69ac3b1b5f9d1e7c2c41c519ff431
heynar-max <heynar76@hotmail.com>
2025-07-14 21:34:33 -0500

    Theme Light y Dark - Generalidades

--- src/presentation/navigation/Navigation.tsx
+++ src/presentation/navigation/Navigation.tsx
@@ -10,6 +10,7 @@ import { CustomSectionListScreen } from '../screens/ui/CustomSectionListScreen';
 import { ModalScreen } from '../screens/ui/ModalScreen';
 import { InfiniteScrollScreen } from '../screens/ui/InfiniteScrollScreen';
 import { SlidesScreen } from '../screens/ui/SlidesScreen';
+import { ChangeThemeScreen } from '../screens/theme/ChangeThemeScreen';
 
 const Stack = createStackNavigator();
 
@@ -32,6 +33,7 @@ export const Navigation = () => {
         <Stack.Screen name="ModalScreen" component={ModalScreen} />
         <Stack.Screen name="InfiniteScrollScreen" component={InfiniteScrollScreen} />
         <Stack.Screen name="SlidesScreen" component={SlidesScreen} />
+        <Stack.Screen name="ChangeThemeScreen" component={ChangeThemeScreen} />
         </Stack.Navigator>
     );
 }
